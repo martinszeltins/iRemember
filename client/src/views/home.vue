17354a5e172995e6d528ed79d1fccbc026519441
Martins <martins@liga-ubuntu.lv>
2020-10-26 22:16:36 +0200

    Navbar part 1

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,5 +1,16 @@
 <template>
-    Home
+    <div class="navbar">
+        <img class="navbar__btn" src="/img/logo-hallow.svg" />
+
+        <div class="navbar__find">
+            <img class="navbar__find-img" src="/img/find.svg">
+            <input class="navbar__find-input" type="text" placeholder="Find" />
+        </div>
+
+        <div class="navbar__username">Mārtiņš Zeltiņš</div>
+
+        <img class="navbar__settings" src="/img/settings.svg" />
+    </div>
 </template>
 
 <script>
